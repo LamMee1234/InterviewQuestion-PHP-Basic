7ed7eabb357c209b061eba5e86e1958450f431a5
LamMee1234 <109353651+LamMee1234@users.noreply.github.com>
2022-12-05 18:23:23 +0800

    Update info.php

---
+++ info.php
@@ -0,0 +1,13 @@
+<?php
+    $username = $_POST['username_php']; 
+    //Post the username data to verify_ajax.js
+    
+    if ($username == "abc") {
+   echo "1";
+   //Return the data is true 
+        }
+   else
+    {
+        echo "0";
+  // Return the data is false
+    }
